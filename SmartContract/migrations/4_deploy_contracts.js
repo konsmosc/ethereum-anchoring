ba891ed139c018cd6a045aa61f2582f627e56abb
bmastahac <bogdan.mastahac@gmail.com>
2020-11-19 10:35:33 +0200

    Added support for smart contract address to be stored in external volume.

--- SmartContract/migrations/4_deploy_contracts.js
+++ SmartContract/migrations/4_deploy_contracts.js
@@ -1,4 +1,6 @@
 const AnchorContract=artifacts.require("./AnchorContract.sol");
 module.exports = function(deployer){
-    deployer.deploy(AnchorContract);
+    deployer.deploy(AnchorContract).then((instance) => {
+        const fs = require('fs').writeFileSync('external/smartcontractaddress.txt',instance.address);
+    });
 };
